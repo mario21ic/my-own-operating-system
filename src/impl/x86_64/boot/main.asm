6e4884421fe7c2368bb79536b0f0a63d71522556
Mario Inga <mario21ic@gmail.com>
2021-05-18 00:11:52 -0500

    feat(global) changing text

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,5 +4,7 @@ section .text
 bits 32
 start:
     ; print `OK`
-    mov dword [0xb8000], 0x2f4b2f4f
+    ;mov dword [0xb8000], 0x2f4b2f4f
+    ;mov dword [0xb8000], 0x2f6b2f4f
+    mov dword [0xb8000], 0x2f4b2f6f
     hlt
