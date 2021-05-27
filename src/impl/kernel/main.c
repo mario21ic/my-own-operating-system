170e621f08c2efdddb0ddac8e312330560d2bf5a
Mario Inga <mario21ic@gmail.com>
2021-05-27 02:30:54 -0500

    feat(global) adding more output

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,8 +1,20 @@
+//#include <stdio.h>
+//#include <sys/types.h>
+//#include <unistd.h>
+
 #include "print.h"
 
 void kernel_main() {
+    // fork(); // we can't run this
+
     print_clear();
+
+    print_set_color(PRINT_COLOR_LIGHT_RED, PRINT_COLOR_BLACK);
+    print_str("UPC - Operating System\n");
+
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("Welcome to our 64-bit kernel!\n");
+
+    print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
     print_str("Let's go");
 }
