8d17564b25f7803151e685451382c0e652106027
Mario Inga <mario21ic@gmail.com>
2021-05-27 01:58:47 -0500

    feat(global) adding Kernel

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # my-own-operating-system
+
+Prerequisites:
+- Docker
+- Qemu
+
+Build and Test:
+```
+./build.sh && ./test.sh
+```
+
+This code is based on https://www.youtube.com/watch?v=FkrpUaGThTQ&list=PLZQftyCk7_SeZRitx5MjBKzTtvk0pHMtp&index=1
--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -e
 
 docker build -t mario21ic/my-own-os:builder buildenv/
-docker run --rm -ti -u $(id -u):$(id -g) -v $PWD:/build/env mario21ic/my-own-os:builder make build-x86_64
+#docker run --rm -ti -u $(id -u):$(id -g) -v $PWD:/build/env mario21ic/my-own-os:builder make build-x86_64
+docker run --rm -ti -v $PWD:/root/env mario21ic/my-own-os:builder make build-x86_64
 
